a28d8159a88e95da5fbdf3d85e40d7a1316cd66a
Greg Plumblee <gplumblee@hotmail.com>
2020-02-06 17:36:01 -0800

    tweaked files

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-let getDate = new Date()
-let date = getDate.getMonth() + "/" + getDate.getDay() + "/" + getDate.getFullYear();
+let d = new Date()
+let date = (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
 console.log(date);
 
 $("button").on("click", function() {
   let city = $("#search-value").val();
   $(".history").append(`<li class="list-group-item">${city}</li>`);
   
-
   let APIKey = "166a433c57516f51dfab1f7edaed8413";
   let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
@@ -21,23 +20,22 @@ $("button").on("click", function() {
 })
 
 function appendWeather(data){
-$("#today").html(`<h1 style="float:left">${data.name} (${date})</h1><img style="float:left" 
-src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"/>`);
-$("#today").append(`<div  class="row" style="float:left; width:100%">
-<div style="width:100%">Temperature: ${Math.round((data.main.temp - 273.15)*1.8 +32)}° F</div>
-<div style="width:100%">Humidity: ${data.main.humidity}</div>
-<div style="width:100%">Wind Speed: ${data.wind.speed}</div>
-<div id="UV" style="width:100%">UV Index: </div>
-<br>
-<h2>5-Day Forecast:</h2>
-</div>`);
+  $("#today").html(`<h1 style="float:left">${data.name} (${date})</h1><img style="float:left" 
+  src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"/>`);
+  $("#today").append(`<div class="row" style="float:left; width:100%">
+  <div style="width:100%">Temperature: ${Math.round((data.main.temp - 273.15)*1.8 +32)}° F</div>
+  <div style="width:100%">Humidity: ${data.main.humidity}</div>
+  <div style="width:100%">Wind Speed: ${data.wind.speed}</div>
+  <div id="UV" style="width:100%">UV Index: </div>
+  </div>
+  <div class="row"><h2>5-Day Forecast:</h2></div>`);
 
-for(let i = 0; i<5;i++){
-  $("#forecast").append(`<div class="col-2">
-    <p>${date}</>
-    <img class="owf owf-803" src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"/>
-    <p>Temp: ${Math.round((data.main.temp - 273.15)*1.8 +32)}° F</p>
-    <p>Humidity: ${data.main.humidity}</p>
-  </div>`);
-}
+  for (let i = 0; i < 5; i++) {
+    $("#forecast").append(`<div class="col-2">
+      <p>${date}</>
+      <img class="owf owf-803" src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"/>
+      <p>Temp: ${Math.round((data.main.temp - 273.15)*1.8 +32)}° F</p>
+      <p>Humidity: ${data.main.humidity}</p>
+    </div>`);
+  }
 }
