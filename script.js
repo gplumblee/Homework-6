d77c206883363800b3984df4f0d924a7db0c8784
Greg Plumblee <gplumblee@hotmail.com>
2020-02-10 18:30:41 -0800

    more tweaks

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ $("button").on("click", function () {
 
     let forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
 
-        $.ajax({
+    $.ajax({
             url: forecastURL,
             method: "GET"
         })
@@ -35,28 +35,27 @@ $("button").on("click", function () {
 })
 
 function appendWeather(data) {
-    $("#today").html(`<h1 style="float:left">${data.name} (${date})</h1><img style="float:left" 
-  src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"/>`);
-    $("#today").append(`<div class="row" style="float:left; width:100%">
-  <div style="width:100%">Temperature: ${Math.round((data.main.temp - 273.15) * 1.8 + 32)}° F</div>
-  <div style="width:100%">Humidity: ${data.main.humidity}</div>
-  <div style="width:100%">Wind Speed: ${data.wind.speed}</div>
-  <div id="UV"></div>
-  </div>
-  <div class="row"><h2>5-Day Forecast:</h2></div>`);
-  lat = data.coord.lat;
-  lon = data.coord.lon;
-  appendUVIndex(lat, lon)
+    $("#today").html(`<div id="main" class="row" style="float:left; width:100%">
+        <h1 style="float:left">${data.name} (${date})</h1><img style="float:left" src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"/>
+        <div style="width:100%">Temperature: ${Math.round((data.main.temp - 273.15) * 1.8 + 32)}° F</div>
+        <div style="width:100%">Humidity: ${data.main.humidity}</div>
+        <div style="width:100%">Wind Speed: ${data.wind.speed}</div>
+        <div id="UV"></div>
+    </div>
+        <div class="row" style="padding-top: 30px; padding-bottom: 0px"><h2>5-Day Forecast:</h2></div>`);
+    lat = data.coord.lat;
+    lon = data.coord.lon;
+    appendUVIndex(lat, lon)
 }
 
 function appendForecast(data) {
     $("#forecast").html("");
     for (let i = 0; i < 40; i += 8) {
         $("#forecast").append(`<div class="col-2">
-      <p>${new Date(data.list[i].dt_txt).toLocaleString()}</>
+      <p style="font-size: 18px; margin-bottom: 0px">${new Date(data.list[i].dt_txt).toLocaleDateString()}</>
       <img class="owf owf-803" src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png"/>
-      <p>Temp: ${Math.round((data.list[i].main.temp - 273.15) * 1.8 + 32)}° F</p>
-      <p>Humidity: ${data.list[i].main.humidity}</p>
+      <p style="font-size: 14px; margin-bottom: 0px">Temp: ${Math.round((data.list[i].main.temp - 273.15) * 1.8 + 32)}° F</p>
+      <p style="font-size: 14px; margin-bottom: 0px">Humidity: ${data.list[i].main.humidity}</p>
     </div>`);
     }
 }
@@ -66,24 +65,24 @@ function appendUVIndex(lat, lon) {
     let UVIndexURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
 
     $.ajax({
-        url: UVIndexURL,
-        method: "GET"
-    })
-    .then(function (response) {
-        console.log(response);
-        // appendUVIndex(response)
-        $("#UV").text(`UV Index: ${response.value}`);
-        if (response.value >= 1 && response.value < 3) {
-            $("#UV").css("background-color", "green");
-        } else if (response.value >= 3 && response.value < 5) {
-            $("#UV").css("background-color", "yellow");
-            $("#UV").css("color", "black");
-        } else if (response.value >= 6 && response.value < 7) {
-            $("#UV").css("background-color", "orange");
-        } else if (response.value >= 8 && response.value < 10) {
-            $("#UV").css("background-color", "red");
-        } else if (response.value >= 11) {
-            $("#UV").css("background-color", "violet");
-        }
-    })
+            url: UVIndexURL,
+            method: "GET"
+        })
+        .then(function (response) {
+            console.log(response);
+            // appendUVIndex(response)
+            $("#UV").text(`UV Index: ${response.value}`);
+            if (response.value >= 1 && response.value < 3) {
+                $("#UV").css("background-color", "green");
+            } else if (response.value >= 3 && response.value < 5) {
+                $("#UV").css("background-color", "yellow");
+                $("#UV").css("color", "black");
+            } else if (response.value >= 6 && response.value < 7) {
+                $("#UV").css("background-color", "orange");
+            } else if (response.value >= 8 && response.value < 10) {
+                $("#UV").css("background-color", "red");
+            } else if (response.value >= 11) {
+                $("#UV").css("background-color", "violet");
+            }
+        })
 }
